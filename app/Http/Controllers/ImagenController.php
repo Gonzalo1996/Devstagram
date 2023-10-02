d980595142f716c75eccd5d6e447817dea1a9061
Gonzalo Abraham Alderete <gonald96@gmail.com>
2023-10-02 20:35:04 -0300

    Save image to folder and create table posts

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request)
     {
-        $imagen = $request->file('file');
+        $image = $request->file('file');
+        $nameImg = Str::uuid() . "." . $image->extension(); //genera un nombre unico 
+        
+        $serverImg = Image::make($image);
+        $serverImg->fit(1000, 1000);
 
-        return response()->json(['imagen' => 'Probando Respuesta']);
+        $pathImg = public_path('uploads') . '/' . $nameImg;
+        $serverImg->save($pathImg);
+
+        return response()->json(['imagen' => $nameImg]);
     }
 }
