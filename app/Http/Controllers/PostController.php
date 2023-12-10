56e41ead4b6f5647523a4c099cadd37cdc3dd5e8
Gonzalo Abraham Alderete <gonald96@gmail.com>
2023-12-10 01:59:45 -0300

    Relationship

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,8 +13,10 @@ class PostController extends Controller
         $this->middleware('auth');
     }
     
-    public function index()
+    public function index(User $user)
     {
+        $posts = Post::where('user_id', $user->id)->get();
+        dd($user->id);
         return view('dashboard');
     }
 
